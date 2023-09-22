8131cd01d384f0d76aa4831217a686ab98ccf846
isaac berlin <isaacmorrisberlin@gmail.com>
2023-09-22 13:24:14 -0500

    basic flask setup

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "test"
+    return render_template('home.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
