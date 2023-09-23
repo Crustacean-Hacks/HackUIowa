ca535035aa1891ceb1b501061ca8ec81ff18d3e1
Andrew O'Konski-Magnuson <okons006@umn.edu>
2023-09-23 14:10:32 -0500

    t

--- extension/api/post.js
+++ extension/api/post.js
@@ -1,8 +1,21 @@
 fetch('http://twitterbecauseitsavailablenow.tech/data_post', {
   method: 'POST',
-  body: JSON.stringify({
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({ // Make sure to stringify your object
     website: 'google.com',
     username: 'i0dev',
     password: 'password'
   })
-});
+})
+.then(response => {
+  if (!response.ok) { // Check if response went through successfully
+    return response.text().then(text => {
+      throw new Error(`Got bad response from server: ${text}`);
+    });
+  }
+  return response.json();
+})
+.then(data => console.log(data))
+.catch((error) => console.log('Error:', error));
