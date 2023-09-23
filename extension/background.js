f272f6daffde1c1bb1cbf94ce163d087fdfff794
isaac berlin <isaacmorrisberlin@gmail.com>
2023-09-23 14:24:07 -0500

    updated important gets

--- extension/background.js
+++ extension/background.js
@@ -2,7 +2,7 @@ function logOpenTabs() {
     // Query for all open tabs
     chrome.tabs.query({}, function (tabs) {
       for (const tab of tabs) {
-        console.log(`Tab ID: ${tab.id}, URL: ${tab.url}, Title: ${tab.title}`);
+        console.log(`URL: ${tab.url}, Favicon: ${tab.favIconUrl}`);
       }
     });
   }
