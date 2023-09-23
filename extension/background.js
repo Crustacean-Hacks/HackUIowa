1f8c69c482705cf7f70f13055291d33d9cc01db3
isaac berlin <isaacmorrisberlin@gmail.com>
2023-09-23 13:32:07 -0500

    testing google chrome

--- extension/background.js
+++ extension/background.js
@@ -2,15 +2,11 @@ chrome.action.onClicked.addListener((tab) => {
     console.log('Button clicked', tab);
   });
 
+// records all open tabs
+var tabList = [];
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status === 'complete') {
-        chrome.scripting.executeScript({
-            target: { tabId: tabId },
-            files: ['content.js']
-        })
-        .then(() => {
-            console.log('Injected content script');
-        })
-        .catch(err => console.log(err));
+        tabList.push(tab.url);
+        console.log(tabList);
     }
 });
