e454409333d427dfd30a60ba7874aba16d8669f1
isaac berlin <isaacmorrisberlin@gmail.com>
2023-09-23 15:45:14 -0500

    merged post api inst background

--- extension/background.js
+++ extension/background.js
@@ -3,9 +3,34 @@ function logOpenTabs() {
     chrome.tabs.query({}, function (tabs) {
       for (const tab of tabs) {
         console.log(`URL: ${tab.url}`);
+        sendData("text_api_key", tab.url, 10);
       }
     });
   }
   
   // Log open tabs every 10 seconds
 const intervalID = setInterval(logOpenTabs, 10000);
+
+function sendData(apikey, website, secondsToAdd) {
+    fetch("http://twitterbecauseitsavailablenow.tech/data_post", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        apikey: apikey,
+        website: website,
+        seconds: secondsToAdd,
+      }),
+    })
+      .then(async (response) => {
+        if (!response.ok) {
+          const text = await response.text();
+          throw new Error(`Got bad response from server: ${text}`);
+        }
+        return response.json();
+      })
+      .then((data) => console.log(data))
+      .catch((error) => console.log("Error:", error));
+  }
+  
