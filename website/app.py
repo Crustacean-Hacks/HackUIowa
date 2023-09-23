21f40f52e121758983366ce74ae517ba5cf5fc65
Andrew O'Konski-Magnuson <okons006@umn.edu>
2023-09-23 18:41:12 -0500

    I  HGATE MY LIFE

--- website/app.py
+++ website/app.py
@@ -78,7 +78,7 @@ def data_post():
         websites = data["websites"]
         seconds = data["seconds"]
 
-        print("Received:" + request.json)
+        print("Received:" + str(equest.json))
 
         DataStore.store(DATABASE, apikey, websites, seconds)
 
