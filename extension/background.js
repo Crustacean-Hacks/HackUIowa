9410e580ecdd9407e8a39d71b954d2bf9bd665fc
isaac berlin <isaacmorrisberlin@gmail.com>
2023-09-23 12:55:59 -0500

    added example tab listener

--- extension/background.js
+++ extension/background.js
@@ -1,4 +1,16 @@
 chrome.action.onClicked.addListener((tab) => {
     console.log('Button clicked', tab);
   });
-  
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status === 'complete') {
+        chrome.scripting.executeScript({
+            target: { tabId: tabId },
+            files: ['content.js']
+        })
+        .then(() => {
+            console.log('Injected content script');
+        })
+        .catch(err => console.log(err));
+    }
+});
