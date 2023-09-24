4ece83865ee62c1a676bd3253a589055d38313ad
Andrew O'Konski-Magnuson <okons006@umn.edu>
2023-09-23 23:59:24 -0500

    new pwompt

--- website/generate_data.py
+++ website/generate_data.py
@@ -153,7 +153,7 @@ def get_category(site):
 # Adds up the times of categories not in the top 5
 def get_rest_data(data):
     min_times = 0
-    for value, key in enumerate(data):
+    for value, _ in enumerate(data):
         min_times += value
     return min_times
 
--- website/site_analysis.py
+++ website/site_analysis.py
@@ -11,7 +11,7 @@ openai.api_key = os.environ.get("OPENAI_API_KEY")
 def categorize_website(site: str) -> str:
     chat = openai.Completion.create(
         model="text-curie-001",
-        prompt="Categorize this website into exactly ONE of the following categories: Ecommerce, Personal, Entertainment, Social, Productivity, Finance, Education, Health, News and Information, or Miscellaneous:"
+        prompt="Categorize this website into exactly ONE of the following categories: [Ecommerce, Personal, Entertainment, Social, Productivity, Finance, Education, Health, News, Information, Miscellaneous]:"
         "" + site + "   ... You MUST choose exactly one category from above.",
     )
     return chat.choices[0].text.strip()
