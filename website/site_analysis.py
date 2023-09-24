81bd73428d28cd0d1af4e360c5d36d03af6ff947
Andrew O'Konski-Magnuson <okons006@umn.edu>
2023-09-23 23:38:23 -0500

    pog

--- website/site_analysis.py
+++ website/site_analysis.py
@@ -11,7 +11,7 @@ openai.api_key = os.environ.get("OPENAI_API_KEY")
 def categorize_website(site: str) -> str:
     chat = openai.Completion.create(
         model="text-curie-001",
-        prompt="Categorize this website into ONE of the following: Ecommerce, personal, entertainment, social, productivity, finance, education, health, news and information, or MISC:"
-        "" + site + "   ... Just give me the top category.",
+        prompt="Categorize this website into ONE of the following: Ecommerce, personal, entertainment, social, productivity, finance, education, health, news and information, or misc:"
+        "" + site + "   ... Just give me the top category. If you cannot categorize something, just give it misc, such as example.com should be misc.",
     )
     return chat.choices[0].text.strip()
