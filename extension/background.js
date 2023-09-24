9ea2add4b83bc485cab325e72dc6242bb7946a95
isaac berlin <isaacmorrisberlin@gmail.com>
2023-09-23 21:48:57 -0500

    fixed api key async problem

--- extension/background.js
+++ extension/background.js
@@ -1,25 +1,35 @@
+api_key = "";
+// get the api key from the storage every 1 minute
+const intervalID2 = setInterval(function () {
+  chrome.storage.sync.get("api_key", function (data) {
+    api_key = data.api_key;
+    console.log(api_key);
+  });
+}, 10000);
+
+// Log open tabs every 10 seconds
+const intervalID = setInterval(logOpenTabs, 10000);
+
 function logOpenTabs() {
   // Query for all open tabs
   chrome.tabs.query({}, function (tabs) {
     // make a list of urls
     var urls = [];
 
-    chrome.storage.sync.get(["api_key"], function (result) {
-      console.log("Value currently is " + result.api_key);
-    });
+    // get api key from storage and save it to api_key 
     for (const tab of tabs) {
       console.log(`URL: ${tab.url}`);
-      if (!tab.url.startsWith("chrome://")) {
-        urls.push(tab.url);
+      if (tab.url.startsWith("chrome://")) {
+        continue;
       }
+      urls.push(tab.url);
     }
-    sendData("test", urls, 10);
+    console.log(urls);
+    console.log(api_key);
+    sendData(api_key, urls, 10);
     urls = [];
   });
 }
-
-// Log open tabs every 10 seconds
-const intervalID = setInterval(logOpenTabs, 10000);
 
 function sendData(apikey, websites, secondsToAdd) {
   fetch("https://twitterbecauseitsavailablenow.tech/data_post", {
