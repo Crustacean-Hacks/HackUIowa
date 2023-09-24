3f67707a733911c8911d6143f9bdf0901a87ff6f
Andrew O'Konski-Magnuson <okons006@umn.edu>
2023-09-24 09:43:29 -0500

    babbage-002

--- website/site_analysis.py
+++ website/site_analysis.py
@@ -10,7 +10,7 @@ openai.api_key = os.environ.get("OPENAI_API_KEY")
 
 def categorize_website(site: str) -> str:
     chat = openai.Completion.create(
-        model="text-curie-001",
+        model="babbage-002",
         prompt="Categorize this website into exactly ONE of the following categories: [Ecommerce, Personal, Entertainment, Social, Productivity, Finance, Education, Health, News, Information, Miscellaneous]:"
         "" + site + "   ... You MUST choose exactly one category from above.",
     )
